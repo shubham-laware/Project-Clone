ade3f2e2dda7d63b9bc3dca3560e292567d9ec4c
Ahmad Munab <ahmadmunab22@gmail.com>
2024-03-20 14:58:29 +0600

    fixing conflicts annd adding shadow

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -2,6 +2,9 @@ import React, { useState } from 'react';
 import { FaLocationDot } from "react-icons/fa6"; 
 import Filter from '../components/Filter';
 import Ban from '../components/images/product.png'
+import { Link } from 'react-router-dom';
+import { products } from '../components/ProductInfo/data.js';
+
 const HomeProducts = () => {
 return(
 <>
@@ -12,41 +15,48 @@ return(
 <br></br>
 <br></br>
 <br></br>
-<div class="container">
+<div className="container">
         <h6>|<FaLocationDot className='fs-3 p-1'/>Find Near You </h6>
-        <div class="row">
+        <div className="row">
            
           <Filter brand="Test" />
              
-            <div class="col-md-10">
-                <div class="row">
-                     
-                    <div class="col-6 col-sm-3 py-2">
-                        <div class="product-card ">
-                            <div class="product-image">
-                                <img src={Ban}   alt="Product 1"/>
-                               
-      
-                             <div class="offer-tag bg-warning rounded-pill text-center p-1 text-light" >25% Off</div>
+            <div className="col-md-10">
+                <div className="row ">
+
+                    {products.map((product,index)=>
+                         <div key={index} className="col-6 col-sm-3 py-2  ">
+                         <div className="product-card  ">
+                             <Link to={`/${product.id}`}>
+                             <div className="product-image">
+                                 <img src={product.imageurl}   alt="Product 1"/>
+                                
        
-   
-                            </div>
-                            <div className='product-content'>
-                            <h6>T-Shirt | Nexa Brand | color: white | other content </h6>
-                            <h5>Price: <sup>&#x20B9;</sup>399<span className='text-decoration-line-through text-muted fs-6 fw-light'>599</span>
-                            <span className='text-muted' style={{fontSize:'13px'}}> In-Stock</span></h5>
-                            <p class="product-rating text-warning">
-                                Rating: &#9733;&#9733;&#9733;&#9733;&#9734;  
-                            </p>
-                            <p class="product-distance text-secondary ">
-                               Distance: 5km away.
-                            </p>
-                            
-                            
-                            </div>
-                        </div>
-                    
-                    </div>
+                              <div className="offer-tag bg-warning rounded-pill text-center p-1 text-light" >{product.percentOff} Off</div>
+        
+    
+                             </div>
+                             <div className='product-content'>
+                             <h6>{product.productTitle} </h6>
+                             <h5>Price: <sup>&#x20B9;</sup>{product.price}<span className='text-decoration-line-through text-muted fs-6 fw-light'>599</span>
+                             <span className='text-muted' style={{fontSize:'13px'}}> {product.stock}</span></h5>
+                             <p className="product-rating text-warning">
+                                 Rating: &#9733;&#9733;&#9733;&#9733;&#9734;  
+                             </p>
+                             <p className="product-distance text-secondary ">
+                                Distance: {product.distance}km away.
+                             </p>
+                             
+                             
+                             </div>
+ 
+                             </Link>
+                         </div>
+                     
+                     </div>
+                    )}
+                     
+                   
                    
                 </div>
             </div>
